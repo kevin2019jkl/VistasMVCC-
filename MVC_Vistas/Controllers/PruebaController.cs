bf3e55571252febcb729cf7d1bcdb0c8642c8de1
Kevin Manuel Carrera Juarez <kcarrera@central.mintrabajo.local>
2019-01-09 11:36:31 -0600

    agregue los primeros archivos

---
+++ MVC_Vistas/Controllers/PruebaController.cs
@@ -0,0 +1,50 @@
+﻿using MVC_Vistas.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Vistas.Controllers
+{
+    public class PruebaController : Controller
+    {
+        // GET: Prueba
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult DatosPersonales(DatosEmail obj)
+        {
+            if (ModelState.IsValid)
+            {
+                String email = obj.email;
+                String password = obj.password;
+                if (email == "kevin@gmail.com" && password == "12345")
+                {
+                    return View();
+                }
+                else
+                {
+                    return View("Index");
+                }
+
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+        public ActionResult MostrarDatos(DatosPersonales obj)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(obj);
+            }
+            else
+            {
+                return View("DatosPersonales");
+            }
+        }
+    }
+}
---
+++ MVC_Vistas/Models/Datos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Vistas.Models
+{
+    public class DatosPersonales
+    {
+        [Required(ErrorMessage="El siguiente dato es requerido")]
+        public String nombre { get; set; }
+
+        [Required(ErrorMessage = "El siguiente dato es requerido")]
+        public String apellido { get; set; }
+
+        [Required(ErrorMessage = "El siguiente dato es requerido")]
+        public String edad { get; set; }
+    }
+    public class DatosEmail
+    {
+        [Required(ErrorMessage = "El email es requerido")]
+        [Display(Name = "Correo Electronico")]
+        public String email { get; set; }
+
+        [Required(ErrorMessage = "La Contraseña es requerida")]
+        [Display(Name = "Contraseña")]
+        public String password { get; set; }
+
+    }
+}
